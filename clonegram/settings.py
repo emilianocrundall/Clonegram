1a47f458cbf7987cd6eaf7a92d86ef355a4f3505
emiliano <crundall01@gmail.com>
2020-06-09 19:03:24 -0300

    update

--- clonegram/settings.py
+++ clonegram/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'red',
         'USER': 'emiliano',
-        'PASSWORD': 'watchitburnpass',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '5432',
     }
